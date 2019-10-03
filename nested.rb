bd8923b2ac4c30def22b38211f2d18ebc3c217f4
tracey brimage <onelovenilynn@gmail.com>
2019-10-03 04:35:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,11 +55,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 
+		 programmer_hash[:dennis_ritchies_language]
 end
 
 def adding_matz
-programmer_hash << ["yukihiro_matsumoto"] [:known_for=> "Ruby"] [:languages=> ["LISP","C"]]
+programmer_hash << ["yukihiro_matsumoto"] 
 
 
 	programmer_hash =
